9c05a1cda6ad113701fc0aec5df7824021571f6a
Malo Bourgon <mbourgon@gmail.com>
2021-03-23 15:59:41 -0700

    Tweak one shot settings

--- users/malob/config.h
+++ users/malob/config.h
@@ -1,5 +1,7 @@
 // These configuration options are applied to all keymaps
 // See option documentation here: https://docs.qmk.fm/#/config_options?id=the-configh-file
 #define TAPPING_TERM 200
-#define ONESHOT_TIMEOUT 300
-#define ONESHOT_TAP_TOGGLE 2
+
+// https://docs.qmk.fm/#/one_shot_keys
+#define ONESHOT_TIMEOUT 500
+#define ONESHOT_TAP_TOGGLE 3
